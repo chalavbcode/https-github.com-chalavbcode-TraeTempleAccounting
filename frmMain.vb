d46c1c4f704f40d207bf18f68547833d4612355d
พระส่่ง <chalalangop@gmail.com>
2026-08-07 20:48:42 +0700

    Increase home logo size to 600x600 in ShowHomeLogo
    The picHomeLogo PictureBox size was updated from 400x400 to 600x600 pixels in the ShowHomeLogo method to improve logo visibility.

--- frmMain.vb
+++ frmMain.vb
@@ -85,7 +85,7 @@ Namespace TempleAccounting
                 picHomeLogo = New PictureBox()
                 picHomeLogo.Image = Image.FromFile(Path.Combine(Application.StartupPath, "Resources\logo_baanchee_wat.png"))
                 picHomeLogo.SizeMode = PictureBoxSizeMode.Zoom
-                picHomeLogo.Size = New Size(400, 400)
+                picHomeLogo.Size = New Size(600, 600)
                 picHomeLogo.BackColor = Color.Transparent
             End If
             picHomeLogo.Location = New Point(
