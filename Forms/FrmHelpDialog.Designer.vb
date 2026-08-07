c0558e2fcfabd0e29abafc6d63dae0783fead4b5
พระส่่ง <chalalangop@gmail.com>
2026-08-07 18:39:54 +0700

    Refactor form layout: unify footer, update header
    Replaced pnlHeader and pnlFooter with pnlBottom for a simplified footer. Docked lblTitle at the top with new styling and padding. Adjusted rtbContent layout and updated control hierarchy and references accordingly.

--- Forms/FrmHelpDialog.Designer.vb
+++ Forms/FrmHelpDialog.Designer.vb
@@ -13,12 +13,10 @@ Namespace TempleAccounting
 
         Private Sub InitializeComponent()
             Me.rtbContent = New System.Windows.Forms.RichTextBox()
-            Me.pnlFooter = New System.Windows.Forms.Panel()
+            Me.pnlBottom = New System.Windows.Forms.Panel()
             Me.btnClose = New System.Windows.Forms.Button()
-            Me.pnlHeader = New System.Windows.Forms.Panel()
             Me.lblTitle = New System.Windows.Forms.Label()
-            Me.pnlFooter.SuspendLayout()
-            Me.pnlHeader.SuspendLayout()
+            Me.pnlBottom.SuspendLayout()
             Me.SuspendLayout()
             '
             'rtbContent
@@ -27,23 +25,23 @@ Namespace TempleAccounting
             Me.rtbContent.BorderStyle = System.Windows.Forms.BorderStyle.None
             Me.rtbContent.Dock = System.Windows.Forms.DockStyle.Fill
             Me.rtbContent.Font = New System.Drawing.Font("Segoe UI", 11.0!)
-            Me.rtbContent.Location = New System.Drawing.Point(20, 70)
+            Me.rtbContent.Location = New System.Drawing.Point(0, 52)
             Me.rtbContent.Name = "rtbContent"
             Me.rtbContent.ReadOnly = True
             Me.rtbContent.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
-            Me.rtbContent.Size = New System.Drawing.Size(760, 430)
+            Me.rtbContent.Size = New System.Drawing.Size(800, 448)
             Me.rtbContent.TabIndex = 0
             Me.rtbContent.Text = ""
             '
-            'pnlFooter
+            'pnlBottom
             '
-            Me.pnlFooter.BackColor = System.Drawing.Color.White
-            Me.pnlFooter.Controls.Add(Me.btnClose)
-            Me.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom
-            Me.pnlFooter.Location = New System.Drawing.Point(0, 500)
-            Me.pnlFooter.Name = "pnlFooter"
-            Me.pnlFooter.Size = New System.Drawing.Size(800, 60)
-            Me.pnlFooter.TabIndex = 1
+            Me.pnlBottom.BackColor = System.Drawing.Color.White
+            Me.pnlBottom.Controls.Add(Me.btnClose)
+            Me.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+            Me.pnlBottom.Location = New System.Drawing.Point(0, 500)
+            Me.pnlBottom.Name = "pnlBottom"
+            Me.pnlBottom.Size = New System.Drawing.Size(800, 60)
+            Me.pnlBottom.TabIndex = 1
             '
             'btnClose
             '
@@ -61,26 +59,19 @@ Namespace TempleAccounting
             Me.btnClose.Text = "ปิดหน้าต่าง"
             Me.btnClose.UseVisualStyleBackColor = False
             '
-            'pnlHeader
-            '
-            Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(73, Byte), Integer), CType(CType(94, Byte), Integer))
-            Me.pnlHeader.Controls.Add(Me.lblTitle)
-            Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
-            Me.pnlHeader.Location = New System.Drawing.Point(0, 0)
-            Me.pnlHeader.Name = "pnlHeader"
-            Me.pnlHeader.Size = New System.Drawing.Size(800, 60)
-            Me.pnlHeader.TabIndex = 2
-            '
             'lblTitle
             '
-            Me.lblTitle.AutoSize = True
+            Me.lblTitle.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(73, Byte), Integer), CType(CType(94, Byte), Integer))
+            Me.lblTitle.Dock = System.Windows.Forms.DockStyle.Top
             Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold)
             Me.lblTitle.ForeColor = System.Drawing.Color.White
-            Me.lblTitle.Location = New System.Drawing.Point(20, 18)
+            Me.lblTitle.Location = New System.Drawing.Point(0, 0)
             Me.lblTitle.Name = "lblTitle"
-            Me.lblTitle.Size = New System.Drawing.Size(185, 25)
-            Me.lblTitle.TabIndex = 0
-            Me.lblTitle.Text = "คู่มือการใช้งานหน้าจอ"
+            Me.lblTitle.Padding = New System.Windows.Forms.Padding(20, 0, 0, 0)
+            Me.lblTitle.Size = New System.Drawing.Size(800, 52)
+            Me.lblTitle.TabIndex = 2
+            Me.lblTitle.Text = "คู่มือการใช้งาน"
+            Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
             '
             'FrmHelpDialog
             '
@@ -89,28 +80,24 @@ Namespace TempleAccounting
             Me.BackColor = System.Drawing.Color.White
             Me.ClientSize = New System.Drawing.Size(800, 560)
             Me.Controls.Add(Me.rtbContent)
-            Me.Controls.Add(Me.pnlHeader)
-            Me.Controls.Add(Me.pnlFooter)
+            Me.Controls.Add(Me.pnlBottom)
+            Me.Controls.Add(Me.lblTitle)
             Me.Font = New System.Drawing.Font("Segoe UI", 9.75!)
             Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
             Me.MaximizeBox = False
             Me.MinimizeBox = False
             Me.Name = "FrmHelpDialog"
-            Me.Padding = New System.Windows.Forms.Padding(20, 10, 20, 0)
             Me.ShowInTaskbar = False
             Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
             Me.Text = "ความช่วยเหลือ - ระบบบัญชีวัด"
-            Me.pnlFooter.ResumeLayout(False)
-            Me.pnlHeader.ResumeLayout(False)
-            Me.pnlHeader.PerformLayout()
+            Me.pnlBottom.ResumeLayout(False)
             Me.ResumeLayout(False)
 
         End Sub
 
         Friend WithEvents rtbContent As System.Windows.Forms.RichTextBox
-        Friend WithEvents pnlFooter As System.Windows.Forms.Panel
+        Friend WithEvents pnlBottom As System.Windows.Forms.Panel
         Friend WithEvents btnClose As System.Windows.Forms.Button
-        Friend WithEvents pnlHeader As System.Windows.Forms.Panel
         Friend WithEvents lblTitle As System.Windows.Forms.Label
     End Class
 End Namespace
